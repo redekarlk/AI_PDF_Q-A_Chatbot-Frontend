64f3c33cd484af7b0ec9a705f86d37122f8688ec
redekarlk <redekarlk123@gmail.com>
2025-12-07 20:20:59 +0530

    backend base url updated

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://ai-pdf-q-a-chatbot-backend.onrender.com",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://ai-pdf-q-a-chatbot-backend-o723.onrender.com",
 });
 
 // attach token automatically
